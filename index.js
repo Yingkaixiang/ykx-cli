84a4e327fea49201beae58ff61d1b41033c873cf
Yingkaixiang <rubykaikai@gmail.com>
2019-03-07 17:08:16 +0800

    feat: 添加 react 组件模板

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = require('inquirer');
 
 const download = require('./download');
 
-program.version('0.0.4', '-v, --version');
+program.version('0.0.5', '-v, --version');
 
 program.command('create <name>').action((name) => {
   inquirer
@@ -39,7 +39,28 @@ program.command('create <name>').action((name) => {
             }
           });
       } else if (answers.type === 'React') {
-        download('github:Yingkaixiang/ykx-template-react', name);
+        inquirer
+          .prompt([
+            {
+              type: 'list',
+              name: 'type',
+              message: '请选择 React 模板',
+              choices: ['web-app', 'component'],
+            },
+          ])
+          .then((answers) => {
+            switch (answers.type) {
+              case 'web-app':
+                download('github:Yingkaixiang/ykx-template-react', name);
+                break;
+              case 'component':
+                download(
+                  'github:Yingkaixiang/ykx-template-react-component',
+                  name,
+                );
+                break;
+            }
+          });
       }
     });
 });
